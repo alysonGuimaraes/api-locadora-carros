de9eace4e1a3a938c20d12ee94446d9528131fab
Alyson Guimaraes <alyson.guima002@gmail.com>
2025-11-29 11:29:07 -0300

    feat: adding endpoint to register a car

---
+++ src/Backend/RentCars.API/Controllers/CarController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentCars.Communication.Requests;
+using RentCars.Communication.Responses;
+
+namespace RentCars.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredCarJson), StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterCarJson request)
+        {
+            return Created();
+        }
+    }
+}
